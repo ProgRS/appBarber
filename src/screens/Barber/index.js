7772e45104a4dc4c65b8f1c46bb9e667c43d3226
ProgRs <lfernandotc@hotmail.com>
2023-02-06 12:46:10 -0300

    Tela do barbeiro 7
    Implementação Tela do barbeiro

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -1,110 +1,194 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from 'react-native';
-import {useNavigation, useRoute} from '@react-navigation/native';
-import { 
-  Container,
-  Scroller,
-  FakeSwiper,
-  PageBody,
-  UserInfoArea,
-  ServiceArea,
-  TestimonialArea,
-  SwipeDot,
-  SwipeDotActive,
-  SwipeItem,
-  SwipeImage,
-  UserAvatar,
-  UserInfo,
-  UserInfoName,
-  UserFavButton
-
-} from './styles';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import Swiper from 'react-native-swiper';
 
-import Api from '../../Api';
-import FavoriteIcon from '../../assets/favorite.svg';
-//import { Alert } from 'react-native/Libraries/Alert/Alert';
-
 import Stars from '../../components/Stars';
-//import { Scroller } from '../Home/styles';
+import BarberModal from '../../components/BarberModal';
 
+import FavoriteFullIcon from '../../assets/favorite_full.svg';
+import FavoriteIcon from '../../assets/favorite.svg';
+import BackIcon from '../../assets/back.svg';
+import NavPrevIcon from '../../assets/nav_prev.svg';
+import NavNextIcon from '../../assets/nav_next.svg';
+
+import {
+    Container,
+    Scroller,
+    PageBody,
+    BackButton,
+    LoadingIcon,
+
+    SwipeDot,
+    SwipeDotActive,
+    SwipeItem,
+    SwipeImage,
+    FakeSwiper,
+
+    UserInfoArea,
+    UserAvatar,
+    UserInfo,
+    UserInfoName,
+    UserFavButton,
+
+    ServiceArea,
+    ServicesTitle,
+    ServiceItem,
+    ServiceInfo,
+    ServiceName,
+    ServicePrice,
+    ServiceChooseButton,
+    ServiceChooseBtnText,
+
+    TestimonialArea,
+    TestimonialItem,
+    TestimonialInfo,
+    TestimonialName,
+    TestimonialBody
+} from './styles';
+
+import Api from '../../Api';
 
 export default () => {
+    const navigation = useNavigation();
+    const route = useRoute();
 
-  const navigation = useNavigation();
-  const route = useRoute();
+    const [userInfo, setUserInfo] = useState({
+        id: route.params.id,
+        avatar: route.params.avatar,
+        name: route.params.name,
+        stars: route.params.stars
+    });
+    const [loading, setLoading] = useState(false);
+    const [favorited, setFavorited] = useState(false);
+    const [selectedService, setSelectedService] = useState(null);
+    const [showModal, setShowModal] = useState(false);
 
-  const [userInfo, setUserInfo] = useState({
-      id: route.params.id,
-      avatar: route.params.avatar,
-      name: route.params.name,
-      stars: route.params.stars
-  });
+    useEffect(()=>{
+        const getBarberInfo = async () => {
+            setLoading(true);
 
-  const [loading, setLoading] = useState(false);
+            let json = await Api.getBarber(userInfo.id);
+            if(json.error == '') {
+                setUserInfo(json.data);
+                setFavorited(json.data.favorited);
+            } else {
+                alert("Erro: "+json.error);
+            }
 
-  useEffect(()=>{
-      const getBarberInfo = async () => {
-        setLoading(true);
-         let json = await Api.getBarber(userInfo.id);
-         if(json.error == ''){
-              setUserInfo(json.data);
-         }else{
-           alert("Erro: "+json.error);
-         }
-         setLoading(false);
-      }
-      getBarberInfo();
-  }, []);
+            setLoading(false);
+        }
+        getBarberInfo();
+    }, []);
 
-  return(
-    <Container>
-      <Scroller>
-        {userInfo.photos && userInfo.photos.length > 0 ?
-          <Swiper
-           style={{height:240}}
-           dot={<SwipeDot/>}
-           activeDot={<SwipeDotActive/>}
-           paginationStyle={{top: 15, right: 15, bottom: null, left:null}}
-           autoplay={true}
-          >
-           {userInfo.photos.map((item, key)=>(
-                <SwipeItem key={key}>
-                    <SwipeImage source={{uri:item.url}} resizeMode="cover"/>
-                </SwipeItem>
-           ))}   
-          </Swiper>
-          : 
-          <FakeSwiper>
+    const handleBackButton = () => {
+        navigation.goBack();
+    }
 
+    const handleFavClick = () => {
+        setFavorited( !favorited );
+        Api.setFavorite( userInfo.id );
+    }
 
+    const handleServiceChoose = (key) => {
+        setSelectedService(key);
+        setShowModal(true);
+    }
 
-          </FakeSwiper>
-      }
-      <PageBody>
-         <UserInfoArea>
+    return (
+        <Container>
+            <Scroller>
+                {userInfo.photos && userInfo.photos.length > 0 ?
+                    <Swiper
+                        style={{height: 240}}
+                        dot={<SwipeDot />}
+                        activeDot={<SwipeDotActive />}
+                        paginationStyle={{top: 15, right: 15, bottom: null, left: null}}
+                        autoplay={true}
+                    >
+                        {userInfo.photos.map((item, key)=>(
+                            <SwipeItem key={key}>
+                                <SwipeImage source={{uri:item.url}} resizeMode="cover" />
+                            </SwipeItem>
+                        ))}
+                    </Swiper>
+                    :
+                    <FakeSwiper></FakeSwiper>
+                }
+                <PageBody>
+                    <UserInfoArea>
+                        <UserAvatar source={{uri:userInfo.avatar}} />
+                        <UserInfo>
+                            <UserInfoName>{userInfo.name}</UserInfoName>
+                            <Stars stars={userInfo.stars} showNumber={true} />
+                        </UserInfo>
+                        <UserFavButton onPress={handleFavClick}>
+                            {favorited ?
+                                <FavoriteFullIcon width="24" height="24" fill="#FF0000" />
+                                :
+                                <FavoriteIcon width="24" height="24" fill="#FF0000" />
+                            }
+                        </UserFavButton>
+                    </UserInfoArea>
 
-          <UserAvatar source={{uri:userInfo.avatar}}/>
-          <UserInfo>
-            <UserInfoName>
-              {userInfo.name}
-            </UserInfoName>
-            <Stars  stars={userInfo.stars} showNumber={true} />
-          </UserInfo>
-            <UserFavButton>
-                <FavoriteIcon width="24" height="24" fill="#FF0000"/>
-            </UserFavButton>
-          
-         </UserInfoArea>
-         <ServiceArea>
+                    {loading &&
+                        <LoadingIcon size="large" color="#000000" />
+                    }
 
-         </ServiceArea>
-         <TestimonialArea>
+                    {userInfo.services &&
+                        <ServiceArea>
+                            <ServicesTitle>Lista de serviços</ServicesTitle>
 
-         </TestimonialArea>
-      </PageBody>
-      </Scroller>
-       
-    </Container>
-  );
+                            {userInfo.services.map((item, key)=>(
+                                <ServiceItem key={key}>
+                                    <ServiceInfo>
+                                        <ServiceName>{item.name}</ServiceName>
+                                        <ServicePrice>R$ {item.price.toFixed(2)}</ServicePrice>
+                                    </ServiceInfo>
+                                    <ServiceChooseButton onPress={()=>handleServiceChoose(key)}>
+                                        <ServiceChooseBtnText>Agendar</ServiceChooseBtnText>
+                                    </ServiceChooseButton>
+                                </ServiceItem>
+                            ))}
+
+                        </ServiceArea>
+                    }
+
+                    {userInfo.testimonials && userInfo.testimonials.length > 0 &&
+                        <TestimonialArea>
+                            <Swiper
+                                style={{height: 110}}
+                                showsPagination={false}
+                                showsButtons={true}
+                                prevButton={<NavPrevIcon width="35" height="35" fill="#000000" />}
+                                nextButton={<NavNextIcon width="35" height="35" fill="#000000" />}
+                            > 
+                                {userInfo.testimonials.map((item, key)=>(
+                                    <TestimonialItem key={key}>
+                                        <TestimonialInfo>
+                                            <TestimonialName>{item.name}</TestimonialName>
+                                            <Stars stars={item.rate} showNumber={false} />
+                                        </TestimonialInfo>
+                                        <TestimonialBody>{item.body}</TestimonialBody>
+                                    </TestimonialItem>
+                                ))}
+                            </Swiper>
+                        </TestimonialArea>
+                    }
+                </PageBody>
+            </Scroller>
+            <BackButton onPress={handleBackButton}>
+                <BackIcon width="44" height="44" fill="#FFFFFF" />
+            </BackButton>
+
+            <BarberModal
+                show={showModal}
+                setShow={setShowModal}
+                user={userInfo}
+                service={selectedService}
+            />
+            
+           
+        </Container>
+    );
 }
