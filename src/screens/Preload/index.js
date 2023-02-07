5d2a5aee3b52b20d61954a8f66e62f406abe35e9
ProgRs <lfernandotc@hotmail.com>
2023-02-07 16:49:42 -0300

    Finalização do App
    Finalização APP barbearia

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,47 +1,56 @@
-import React, {useEffect, useContext} from "react";
+
+//import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import React, { useEffect, useContext } from 'react';
 import { Container, LoadingIcon } from './styles';
+//import AsyncStorage from '@react-native-community/async-storage';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
-import { UserContext } from "../../contexts/UserContext";
-//import { Text } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+import { UserContext } from '../../contexts/UserContext';
+import Api from '../../Api';
+
 import BarberLogo from '../../assets/barber.svg';
 
-import Api from "../../Api";
 export default () => {
 
-  const { dispatch: userDispatch} = useContext(UserContext);
-  const navigation = useNavigation();
+    const { dispatch: userDispatch } = useContext(UserContext);
+    const navigation = useNavigation();
 
-  useEffect(()=>{
-          const checkToken = async () =>{
-                const token = await AsyncStorage.getItem('token');
-                if(token != null) {
-                    let res = await Api.checkToken(token); 
-                    if(res.token){
-                      await AsyncStorage.setItem('token', json.token);
-                      userDispatch({
-                           type: 'setAvatar',
-                           payload:{
-                              avatar: json.data.avatar
-                           }
-                      });
-             
-                      navigation.reset({
-                           routes:[{name:'MainTab'}]
-                      });
-                    } else{
-                      navigation.navigate('SignIn');
-                    } 
-                }else{
+    useEffect(()=>{
+        const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token');
+            if(token) {
+                let res = await Api.checkToken(token);
+                if(res.token) {
+
+                    await AsyncStorage.setItem('token', res.token);
+
+                    userDispatch({
+                        type: 'setAvatar',
+                        payload:{
+                            avatar: res.data.avatar
+                        }
+                    });
+
+                    navigation.reset({
+                        routes:[{name:'MainTab'}]
+                    });
+
+                } else {
                     navigation.navigate('SignIn');
                 }
-          }
-          checkToken();
-  },[]);
-  return(
-    <Container>
-        <BarberLogo width="100%" height="160"/>
-        <LoadingIcon size="large" color="#FFFFFF"/>
-    </Container>
-  );
+            } else {
+                navigation.navigate('SignIn');
+            }
+        }
+        checkToken();
+    }, []);
+
+    return (
+        <Container>
+            <BarberLogo width="100%" height="160" />
+            <LoadingIcon size="large" color="#FFFFFF" />
+        </Container>
+    );
 }
--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,106 +1,104 @@
 
 //Tela de Login
-import React , {useState, useContext} from "react";
-import { useNavigation } from "@react-navigation/native";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { UserContext } from "../../contexts/UserContext";
-import { 
-  Container,
-  InputArea,
-  CustomButton,
-  CustomButtonText,
-  SignMessageButton,
-  SignMessageButtonText,
-  SignMessageButtonTextBold
 
+
+import React, { useState, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
+//import AsyncStorage from '@react-native-community/async-storage';
+
+//import { UserContext } from '../../contexts/UserContext';
+
+import {
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
 } from './styles';
 
+import Api from '../../Api';
 
-import Api from "../../Api";
-import { Text } from "react-native";
-
-import SignInput from "../../components/SignInput";
-
+import SignInput from '../../components/SignInput';
 
 import BarberLogo from '../../assets/barber.svg';
 import EmailIcon from '../../assets/email.svg';
 import LockIcon from '../../assets/lock.svg';
 
-
-
 export default () => {
+    const { dispatch: userDispatch } = useContext(UserContext);
+    const navigation = useNavigation();
 
-  const navigation = useNavigation();
-  const { dispatch: userDispatch} = useContext(UserContext);
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
 
-  const [emailField , setEmailField] = useState('');
-  const [passwordField , setPasswordField] = useState('');
+    const handleSignClick = async () => {
+        if(emailField != '' && passwordField != '') {
 
+            let json = await Api.signIn(emailField, passwordField);
 
+            if(json.token) {
+                await AsyncStorage.setItem('token', json.token);
 
- const handleSignClick = async () => {
-  if(emailField != '' && passwordField != '') {
+                userDispatch({
+                    type: 'setAvatar',
+                    payload:{
+                        avatar: json.data.avatar
+                    }
+                });
 
-      let json = await Api.signIn(emailField, passwordField);
+                navigation.reset({
+                    routes:[{name:'MainTab'}]
+                });
+            } else {
+                alert('E-mail e/ou senha errados!');
+            }
 
-      if(json.token) {
-         await AsyncStorage.setItem('token', json.token);
-         userDispatch({
-              type: 'setAvatar',
-              payload:{
-                 avatar: json.data.avatar
-              }
-         });
+        } else {
+            alert("Preencha os campos!");
+        }
+    }
 
-         navigation.reset({
-              routes:[{name:'MainTab'}]
-         });
-          
-      } else {
-          alert('E-mail e/ou senha errados!');
-      }
+    const handleMessageButtonClick = () => {
+        navigation.reset({
+            routes: [{name: 'SignUp'}]
+        });
+    }
 
-  } else {
-      alert("Preencha os campos!");
-  }
-}
+    return (
+        <Container>
+            <BarberLogo width="100%" height="160" />
 
-  const handleMessageButtonClick = () => {
-  navigation.reset({
-      routes: [{name: 'SignUp'}]
-  });
-}
+            <InputArea>
+                <SignInput
+                    IconSvg={EmailIcon}
+                    placeholder="Digite seu e-mail"
+                    value={emailField}
+                    onChangeText={t=>setEmailField(t)}
+                />
 
-  return(
-    <Container>
-       <BarberLogo width="100%" height="160"/>
+                <SignInput
+                    IconSvg={LockIcon}
+                    placeholder="Digite sua senha"
+                    value={passwordField}
+                    onChangeText={t=>setPasswordField(t)}
+                    password={true}
+                />
 
-       <InputArea> 
-          <SignInput 
-          IconSvg={EmailIcon}
-          placeholder="Digite seu e-mail"
-          value={emailField}
-          onChangeText={t=>setEmailField(t)}
-          
-          />
-          <SignInput 
-          IconSvg={LockIcon}
-          placeholder="Digite sua senha"
-          value={passwordField}
-          onChangeText={t=>setPasswordField(t)}
-          password={true}
-          />
+                <CustomButton onPress={handleSignClick}>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+            </InputArea>
 
-          <CustomButton onPress={handleSignClick} >
-            <CustomButtonText>LOGIN</CustomButtonText>
-          </CustomButton>
-       </InputArea>
+            <SignMessageButton onPress={handleMessageButtonClick}>
+                <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+            </SignMessageButton>
 
-        <SignMessageButton onPress={handleMessageButtonClick}>
-            <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
-            <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
-        </SignMessageButton>
-
-    </Container>
-  );
+        </Container>
+    );
 }
